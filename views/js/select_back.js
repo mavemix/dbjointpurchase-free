b9102cf94817831acbd8c08b0e7f77c877db6117
Andrés <andres.alvarez.alvarez@gmail.com>
2023-04-24 09:17:16 +0200

    Construimos con JS el listado de productos seleccionables

--- views/js/select_back.js
+++ views/js/select_back.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
     let tab = $('#related-product').parent();
     
-    let parentDiv1 = $('<h3>COMPRADOS JUNTOS HABITUALMENTE</h3>');
+    let titulo = $('<h2>Comprados juntos habitualmente</h2>');
+
+    let container = $('<ul class="category-tree">'+product+'</ul>');
+
+    let contenido = "";
+
+    products_cat.forEach(products => {
+        products.forEach(product => {
+            contenido += '<li><div class="checkbox"><label><input class = "jointCheckbox" type="checkbox" ';
+            contenido += 'value="' + product.id_product + '">' + product.name;
+            contenido += '</label></div></li>';
+        })
+    });
     
-    tab.append(parentDiv1);
-});
+    tab.append(titulo);
+    tab.append(container.append(contenido));
 
