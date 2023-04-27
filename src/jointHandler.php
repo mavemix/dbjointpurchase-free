f68f261170f43fbfd4c2ca5002238a47561d2b30
Andrés <andres.alvarez.alvarez@gmail.com>
2023-04-27 09:59:05 +0200

    Escapamos las clases de Prestashop al namespace

--- src/jointHandler.php
+++ src/jointHandler.php
@@ -2,7 +2,7 @@
 
 namespace mavemix\dbjointpurchase;
 
-use Db;
+
 
 class JointHandler 
 {
@@ -10,18 +10,54 @@ class JointHandler
      * Dado un producto, obtiene los productos adheridos manualmente por el usuario
      * 
      * @param $id_product   El id de producto a consultar
+     * @param $front        Indica si el restultado debe mostrarse en el formato usado en el front para construir los productos
      * 
      * @return array|false  El registro actual en BD, o false si no existiese
      */
-    public static function getJointsByProduct($id_product)
+    public static function getJointsByProduct($id_product, $front = false)
     {
         $sql = "SELECT id_joint1, id_joint2, id_joint3 FROM `" . _DB_PREFIX_ . "dbjointpurchase_joints` WHERE id_product = ".$id_product;
-        $results = Db::getInstance()->getRow($sql);
-            if (!$results) {
-                return false;
-            }
-            
+        $results = array_filter(\Db::getInstance()->getRow($sql));
+        
+        if (!$results) {
+            return false;
+        }
+
+        if(!$front) {
             return $results;
+        }
+
+        $sql = "SELECT od.product_id, count(od.product_id) as total, p.price, p.id_category_default
+        FROM " . _DB_PREFIX_ . "order_detail od
+        LEFT JOIN " . _DB_PREFIX_ . "product p ON od.product_id = p.id_product
+        LEFT JOIN " . _DB_PREFIX_ . "product_shop ps ON od.product_id = ps.id_product
+        " . \Shop::addSqlAssociation('product', 'p') . "
+        " . \Product::sqlStock('p', 0) . "
+        WHERE ps.active = 1 
+            AND p.available_for_order = 1  
+            AND p.visibility != 'none' 
+            AND p.price > 0
+            AND (stock.out_of_stock = 1 OR stock.quantity > 0)";
+    if (!empty($excludes)) {
+        $sql .= " AND od.product_id NOT IN (" . $excludes . ")";
+    }
+    $sql .= " GROUP BY od.product_id 
+        HAVING COUNT(*) > 1 
+        ORDER BY total DESC
+        LIMIT 3";
+    $results = \Db::getInstance()->ExecuteS($sql);
+    if (count($results) >= 1) {
+        foreach ($results as $key => $row) {
+            $products[$key][] = array(
+                'id_product' => $row['product_id'],
+                'price' => $row['price'],
+            );
+        }
+        return $products;
+    }
+    return false;
+
+            
     }
 
     /**
@@ -40,7 +76,7 @@ class JointHandler
             $sql = "INSERT INTO `" . _DB_PREFIX_ . "dbjointpurchase_joints` 
                     (`id_product`, `id_joint1`)
                     VALUES ('".$id_product."', '".$id_joint."')";
-            if (Db::getInstance()->execute($sql) == false) {
+            if (\Db::getInstance()->execute($sql) == false) {
                 return false;
             }
             return true;
@@ -74,7 +110,7 @@ class JointHandler
         foreach($joints as $index => $joint) {
             if($joint == 0) {
                 $sql = "UPDATE `" . _DB_PREFIX_ . "dbjointpurchase_joints` SET `".$index."`  = ".$id_joint." WHERE `id_product`= ".$id_product;
-                return Db::getInstance()->execute($sql);
+                return \Db::getInstance()->execute($sql);
             }
         }
         return false;
@@ -89,11 +125,11 @@ class JointHandler
             if( ($joint == $id_joint) && (count(array_filter($joints))==1) ) {
                 $sql = "DELETE FROM `" . _DB_PREFIX_ . "dbjointpurchase_joints` 
                         WHERE `" . _DB_PREFIX_ . "dbjointpurchase_joints`.`id_product` = ".$id_product;
-                return Db::getInstance()->execute($sql);
+                return \Db::getInstance()->execute($sql);
             }
             if($joint == $id_joint) {
                 $sql = "UPDATE `" . _DB_PREFIX_ . "dbjointpurchase_joints` SET `".$index."`  = 0";
-                return Db::getInstance()->execute($sql);
+                return \Db::getInstance()->execute($sql);
             }
         }
         return false;
